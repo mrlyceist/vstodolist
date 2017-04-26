93cbc554667e42e44dcbe3b4d33819bca200e71b
mrlyceist <dorogov.alexey@gmail.com>
2017-04-27 03:35:32 +0500

    Added ViewModel class, implemented custom Command class

---
+++ todolist/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using todolist.Annotations;
+
+namespace todolist
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly IFileService _fileService;
+
+        //private TodoItem _selectedItem;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
