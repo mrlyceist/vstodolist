7d9bdb241b65814a2ba3e687d86ea335191707b3
mrlyceist <dorogov.alexey@gmail.com>
2017-04-27 05:01:42 +0500

    Implemented converters.

---
+++ todolist/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace todolist.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        private static BoolToVisibilityConverter _instance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = System.Convert.ToBoolean(parameter);
+            if (!flag)
+                return (bool)value == false ? Visibility.Hidden : Visibility.Visible;
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new BoolToVisibilityConverter());
+        }
+    }
+}
