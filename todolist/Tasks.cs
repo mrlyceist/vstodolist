b03dd20ed08d926b12df649ddfc4723c42bb464e
mrlyceist <dorogov.alexey@gmail.com>
2017-04-27 11:58:36 +0500

    Implemented Serializer class and wrapper class

---
+++ todolist/Tasks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace todolist
+{
+    [XmlRoot("Tasks")]
+    internal class Tasks
+    {
+        [XmlElement("item")]
+        public List<TodoItem> Items { get; set; }
+    }
+}
--- todolist/XmlFileService.cs
+++ todolist/XmlFileService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace todolist
 {
@@ -6,12 +8,24 @@ namespace todolist
     {
         public List<TodoItem> Open(string fileName)
         {
-            throw new System.NotImplementedException();
+            List<TodoItem> items = new List<TodoItem>();
+            XmlSerializer serializer = new XmlSerializer(typeof(Tasks));
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                var itemList = serializer.Deserialize(fs) as Tasks;
+                items = itemList?.Items;
+            }
+            return items;
         }
 
         public void Save(string fileName, List<TodoItem> items)
         {
-            throw new System.NotImplementedException();
+            var itemList = new Tasks { Items = items };
+            XmlSerializer serializer = new XmlSerializer(typeof(Tasks));
+            using (var fs = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.Serialize(fs, itemList);
+            }
         }
     }
 }
