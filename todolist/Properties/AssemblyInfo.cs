9ac9e146ade8ada163d2e075c89641d392a640dd
mrlyceist <dorogov.alexey@gmail.com>
2017-04-28 04:05:27 +0500

    Removed commented code.

--- todolist/Properties/AssemblyInfo.cs
+++ todolist/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Todo List")]
-[assembly: AssemblyDescription("Simple yet fancy Todo List extension for VS2015")]
+[assembly: AssemblyDescription("Simple yet fancy Todo List extension for VS2017 (VS2015 compatible)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TodoList")]
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.29")]
-[assembly: AssemblyFileVersion("1.0.2.29")]
+[assembly: AssemblyVersion("1.2.2.29")]
+[assembly: AssemblyFileVersion("1.2.2.29")]
 [assembly: NeutralResourcesLanguage("")]
 
